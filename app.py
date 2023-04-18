b0c7405545239d0f5081a3cfad28d310b7c5a192
Jonathan Smitter <jsmitterc@gmail.com>
2023-04-18 09:59:31 -0400

    app.py fix

--- app.py
+++ app.py
@@ -9,14 +9,29 @@ from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 class Email(BaseModel):
     email: str
     sales_id: int
 
+origins = ["*"]
+
+
+
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
